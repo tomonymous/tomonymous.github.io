2b84a6e90cb9729da71477e7331ff0eb4aed70a4
tomonymous <tjcheung@gmail.com>
2023-06-20 22:17:20 +0200

    Added a lightbox to the Photography section.

--- src/components/Photography.js
+++ src/components/Photography.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Gallery from "react-photo-gallery";
 import { photos } from "../photos";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import { render } from "react-dom";
+import Carousel, { Modal, ModalGateway } from "react-images";
 
 export const Photography = () => {
+
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+  
+    const openLightbox = useCallback((event, { photo, index }) => {
+      setCurrentImage(index);
+      setViewerIsOpen(true);
+    }, []);
+  
+    const closeLightbox = () => {
+      setCurrentImage(0);
+      setViewerIsOpen(false);
+    };
 
     const containerStyle = {
         height: "60vh",
@@ -25,7 +40,21 @@ export const Photography = () => {
                 </TrackVisibility>
                 <Col>
                     <div style={containerStyle}>
-                        <Gallery photos={photos} direction="column" columns={4} />
+                    <Gallery photos={photos} onClick={openLightbox} direction="column" columns={4}/>
+                    <ModalGateway>
+                        {viewerIsOpen ? (
+                        <Modal onClose={closeLightbox}>
+                            <Carousel
+                            currentIndex={currentImage}
+                            views={photos.map(x => ({
+                                ...x,
+                                srcset: x.srcSet,
+                                caption: x.title
+                            }))}
+                            />
+                        </Modal>
+                        ) : null}
+                    </ModalGateway>
                     </div>
                 </Col>
             </Container>
